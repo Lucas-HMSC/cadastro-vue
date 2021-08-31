af7d7ffe439f6fac35e03659c14fbc8471cadeb8
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-31 14:25:41 -0300

    Component Wrapper

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrapper">
+    <section class="sidebar-left">
+      <h1>Learn to code by watching others</h1>
+      <p>see how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+    </section>
+    <section class="sidebar-right">
+      <InputPromo />
+      <slot></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+import InputPromo from '@/components/InputPromo.vue';
+
+export default {
+  name: 'Wrapper',
+  components: {
+    InputPromo
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: 100vh;
+  max-width: 60rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+
+  .sidebar-left {
+    color: #eee;
+    display: flex;
+    flex-direction: column;
+    max-width: 30rem;
+    gap: 2rem;
+
+    h1 {
+      font-size: 2.5rem;
+      max-width: 22rem;
+    }
+  }
+
+  .sidebar-right {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    max-width: 30rem;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,18 @@
 <template>
-  <div class="register">
-    <section class="sidebar-left">
-      <h1>Learn to code by watching others</h1>
-      <p>see how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
-    </section>
-    <section class="sidebar-right">
-      <InputPromo />
-      <LoginSuccess />
-    </section>
-  </div>
+  <Wrapper>
+    <LoginSuccess />
+  </Wrapper>
 </template>
 
 <script>
+import Wrapper from '@/components/Wrapper.vue';
 import LoginSuccess from '@/components/LoginSuccess.vue';
-import InputPromo from '@/components/InputPromo.vue';
 
 export default {
-  name: 'Home',
+  name: 'Register',
   components: {
-    LoginSuccess,
-    InputPromo,
-  },
+    Wrapper,
+    LoginSuccess
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-.register {
-  width: 100%;
-  height: 100vh;
-  max-width: 60rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-
-  .sidebar-left {
-    color: #eee;
-    display: flex;
-    flex-direction: column;
-    max-width: 30rem;
-    gap: 2rem;
-
-    h1 {
-      font-size: 2.5rem;
-      max-width: 22rem;
-    }
-  }
-
-  .sidebar-right {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    max-width: 30rem;
-  }
-}
-</style>
